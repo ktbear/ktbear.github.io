0069c9ef36c3209775ba39a1c670ae0079441250
15182089135 <1204957204@qq.com>
2020-04-05 20:22:59 +0800

    Site updated: 2020-04-05 20:22:51

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 04 2020 19:07:55 GMT+0800 (GMT+08:00)
-const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"张泽历",artist:"迪士尼在逃公主",url:"https://sharefs.yun.kugou.com/202004031504/2bac7dededabb053f1774a8b7b5e87bc/G202/M05/14/0B/qpQEAF5xIjCACNWeADQs6LKhWMc004.mp3",cover:"https://picsum.photos/100/100?image=1082"},{name:"Drip Drip Drip",artist:"植地雅哉",url:"https://sharefs.yun.kugou.com/202004031927/5ba1cfa7fde09e616687216fd73dcf61/G156/M07/1B/09/PIcBAFyrA9CAHtZRAGFh_Dxw-FI867.mp3",cover:"https://picsum.photos/100/100?image=1005"}]});
+// build time:Sun Apr 05 2020 20:22:37 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"徐秉龙",artist:"迪士尼在逃公主",url:"http://music.163.com/song/media/outer/url?id=1397679310.mp3",cover:"https://picsum.photos/100/100?image=1082"},{name:"心似烟火",artist:" 陈壹千",url:"http://music.163.com/song/media/outer/url?id=1399112638.mp3",cover:"https://picsum.photos/100/100?image=1005"}]});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 04 2020 19:07:55 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 05 2020 20:22:37 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
