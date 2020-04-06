5bc9926cc4937d25b60c551d70aabc611f1881f1
15182089135 <1204957204@qq.com>
2020-04-06 21:24:42 +0800

    Site updated: 2020-04-06 21:24:34

--- static/api/js/trans/trans_bdxc.js
+++ static/api/js/trans/trans_bdxc.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 06 2020 11:29:59 GMT+0800 (GMT+08:00)
+// build time:Mon Apr 06 2020 20:35:03 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("trans/trans_bdxc",function(e,t){var n=function(){var e=window,t=document,n="_bdXC",a;e[n]?window._bdXC_loaded&&e[n].reInit():(a=t.createElement("script"),a.setAttribute("charset","utf-8"),a.src="http://xiangce.baidu.com/zt/collect/mark.js?"+(new Date).getTime(),t.getElementsByTagName("head")[0].appendChild(a))};t.run=n});
 //rebuild by neat 
