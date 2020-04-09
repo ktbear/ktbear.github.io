e49b2e8bfb8847158e349d05c06ac76c30147eb3
15182089135 <1204957204@qq.com>
2020-04-09 21:21:47 +0800

    Site updated: 2020-04-09 21:21:39

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 09 2020 21:00:48 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 09 2020 21:21:25 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
