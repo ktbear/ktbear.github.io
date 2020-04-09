3e98fe7774d536ae7008fafa0e25112db0002263
15182089135 <1204957204@qq.com>
2020-04-09 21:20:10 +0800

    Site updated: 2020-04-09 21:20:01

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 08 2020 14:17:09 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 09 2020 21:00:47 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"徐秉龙",artist:"迪士尼在逃公主",url:"http://music.163.com/song/media/outer/url?id=1397679310.mp3",cover:"https://picsum.photos/100/100?image=1082"},{name:"心似烟火",artist:" 陈壹千",url:"http://music.163.com/song/media/outer/url?id=1399112638.mp3",cover:"https://picsum.photos/100/100?image=1005"}]});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 08 2020 14:17:10 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 09 2020 21:00:48 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
