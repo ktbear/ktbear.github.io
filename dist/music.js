9ec8de991a140a610f108027bf6b61ced17fb9f3
15182089135 <1204957204@qq.com>
2020-05-15 13:54:15 +0800

    Site updated: 2020-05-15 13:54:07

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Fri May 15 2020 12:26:31 GMT+0800 (GMT+08:00)
-const ap=new APlayer({container:document.getElementById("aplayer"),theme:"#9370DB",fixed:true,autoplay:false,preload:true,audio:[{name:"心似烟火",artist:" 陈壹千",url:"http://music.163.com/song/media/outer/url?id=1399112638.mp3",cover:"https://picsum.photos/100/100?image=1005"},{name:"徐秉龙",artist:"迪士尼在逃公主",url:"http://music.163.com/song/media/outer/url?id=1397679310.mp3",cover:"https://picsum.photos/100/100?image=1082"}]});
+// build time:Fri May 15 2020 13:00:18 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),theme:"#9370DB",fixed:true,autoplay:false,preload:true,audio:[{name:"心似烟火",artist:" 陈壹千",url:"http://music.163.com/song/media/outer/url?id=1399112638.mp3",cover:"http://ww1.sinaimg.cn/large/006brE51gy1geq08gz2gnj30u80kuq66.jpg"},{name:"徐秉龙",artist:"迪士尼在逃公主",url:"http://music.163.com/song/media/outer/url?id=1397679310.mp3",cover:"http://ww1.sinaimg.cn/large/006brE51gy1geq08gz2gnj30u80kuq66.jpg"}]});
 //rebuild by neat 
